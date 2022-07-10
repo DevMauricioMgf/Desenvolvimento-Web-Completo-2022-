1b8d146da993cd5717e5da6de1784f3adba70141
Mauricio Friedrich <mauriciofriedrich.98@gmail.com>
2022-07-10 17:15:34 -0300

    include, include_once, require, require_once

---
+++ PHP/incorporando_scripts/inicio.php
@@ -0,0 +1,16 @@
+<?php
+    require("menu.php"); // caso não ache ele gera um warning
+    //require              //fatal error/ interrompe atividades     posteriores
+
+?>
+
+
+
+
+conteudo da página (início)
+<br>
+<?php
+    require("menu.php"); // caso não ache ele gera um warning
+    //require              //fatal error/ interrompe atividades     posteriores
+
+?>
---
+++ PHP/incorporando_scripts/minha_rede.php
@@ -0,0 +1,8 @@
+<?php
+    include("menu.php");
+
+
+?>
+
+
+conteudo da página (minha rede)
